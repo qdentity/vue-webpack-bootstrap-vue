9f745e673fa581a817c87d02ab8a2d2ae49fca67
Jan-Willem <jan-willem@qdentity.nl>
2017-09-15 15:23:34 +0200

    initial commit - BootstrapVue support
    https://bootstrap-vue.js.org

--- template/src/main.js
+++ template/src/main.js
@@ -2,13 +2,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
+import '@/style/index.scss'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import BootstrapVue from 'bootstrap-vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{#router}}
 import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
 
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.use(BootstrapVue){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 /* eslint-disable no-new */
 new Vue({
