3535b1c4f5324ba010dbffa712f5c68ec1d79c08
Jan-Willem <jan-willem@qdentity.nl>
2017-10-05 15:43:31 +0200

    vuex, js-logger support

--- template/src/main.js
+++ template/src/main.js
@@ -2,15 +2,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
-import '@/style/index.scss'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import BootstrapVue from 'bootstrap-vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Logger from 'js-logger'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{#router}}
 import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
+import '@/style/index.scss'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+Logger.useDefaults(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+if (process.env.NODE_ENV === 'production') {
+  Logger.setLevel(Logger.OFF){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+}
 
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 Vue.use(BootstrapVue){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 /* eslint-disable no-new */
@@ -19,6 +27,7 @@ new Vue({
   {{#router}}
   router,
   {{/router}}
+  // CAUTION: when using vuex store, please use -> strict: process.env.NODE_ENV !== 'production',
   {{#if_eq build "runtime"}}
   render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
   {{/if_eq}}
